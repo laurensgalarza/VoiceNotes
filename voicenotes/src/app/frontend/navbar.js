c85bbf50f89fcdb105203af76b0ccfd37ebb12c2
KenJiang10 <TheKenJiang10@gmail.com>
2025-05-16 02:39:07 -0400

    nav

--- voicenotes/src/app/frontend/navbar.js
+++ voicenotes/src/app/frontend/navbar.js
@@ -3,11 +3,16 @@
 import React from "react";
 
 export default function NavBar() {
+  return (
+    <nav className="flex justify-between px-10 py-6 bg-white">
+      {/* voicenotes */}
+      <div className="text-xl font-medium">VoiceNotes</div>
 
-    return (
-        <div>
-            
-        </div>
-    )
-
+      {/* links */}
+      <div className="space-x-6 text-base">
+        <a href="/about" className="hover:underline">About</a>
+        <a href="/upload" className="hover:underline">Upload</a>
+      </div>
+    </nav>
+  );
 }
--- voicenotes/src/app/page.js
+++ voicenotes/src/app/page.js
@@ -7,24 +7,15 @@ import Result from "./frontend/result";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <nav>
-        <NavBar/>
-      </nav>
-      
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        
-        <Landing/>
-        <About/>
-        <Create/>
-        <Result/>
+    <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <NavBar />
 
+      <main className="flex flex-col gap-[35px] row-start-2 items-center sm:items-start">
+        <Landing />
+        <About />
+        <Create />
+        <Result />
       </main>
-        
-
-
-
-\
     </div>
   );
 }
