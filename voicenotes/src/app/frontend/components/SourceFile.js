ecb9e658c8cd8f939482b1850c4c9fc19c089a8d
AbdulRK23 <128845322+AbdulRK23@users.noreply.github.com>
2025-05-20 03:18:57 -0400

    Updated SourceFile.js

--- voicenotes/src/app/frontend/components/SourceFile.js
+++ voicenotes/src/app/frontend/components/SourceFile.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { X } from 'lucide-react';
 
-export default function SourceFile(file) {
-    //filepreview is like a few lines of text from the file
-
+export default function SourceFile({ filename, filepreview }) {
     return (
         <div className="space-y-4">
             <div className="bg-white rounded-lg shadow p-4">
-                <h3 className="font-semibold">{fileDetails.filename}</h3>
-                <p className="text-sm text-gray-500">Parsed Text:<br />{fileDetails.filepreview}</p>
+                <h3 className="font-semibold">{filename}</h3>
+                <p className="text-sm text-gray-500">
+                    Parsed Text:<br />{filepreview}
+                </p>
             </div>
         </div>
-    )
-
+    );
 }
