4ce38a31c402c3d6e1d66bddcf8883ce8ff91555
laurensgalarza <92804326+laurensgalarza@users.noreply.github.com>
2025-05-19 20:42:59 -0400

    messed up merging but everything is good now

--- voicenotes/src/app/frontend/result.js
+++ voicenotes/src/app/frontend/result.js
@@ -6,11 +6,11 @@ export default function Result({story}) {
 
   return (
     
-    <div className="container mx-auto max-w-2xl">       {/* text transcription container*/}
-      <h1 className="mb-5 text-center text-5xl font-semibold text-gray-800">
+    <div className="mt-50 w-[75%] md:w-full container mx-auto max-w-2xl">       {/* text transcription container*/}
+      <h1 className="mb-5 text-4xl md:text-5xl text-center font-semibold text-gray-800">
         Your Study Podcast
       </h1>
-      <div className="bg-gray-200 px-18 pt-10 pb-5 rounded-4xl mb-8 text-gray-700 leading-relaxed text-left">
+      <div className="bg-gray-200 px-6 md:px-18 pt-6 md:pt-10 pb-5 rounded-2xl md:rounded-4xl mb-8 text-gray-700 leading-relaxed text-left">
         {story ? (<p className="mb-4">{story}</p>) : 
         (<div><p className="mb-4">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus metus et
@@ -54,8 +54,8 @@ export default function Result({story}) {
           Upload more files
         </button>
       </div>
-      <p className="text-3xl text-gray-700 m-20 font-semibold text-center">
-        Thanks for using VoiceNotes
+      <p className="text-4xl text-gray-700 m-30 font-semibold text-center">
+        Thanks for using VoiceNotes!
       </p>
     </div>
   );
