d1a3058a0fceaf04c207077850022f73e3be6cc3
AbdulRK23 <128845322+AbdulRK23@users.noreply.github.com>
2025-05-20 05:03:16 -0400

    Updated SourceFile.js

--- voicenotes/src/app/frontend/components/SourceFile.js
+++ voicenotes/src/app/frontend/components/SourceFile.js
@@ -1,14 +1,13 @@
+// components/SourceFile.js
 import React from "react";
 
 export default function SourceFile({ filename, filepreview }) {
     return (
-        <div className="space-y-4">
-            <div className="bg-white rounded-lg shadow p-4">
-                <h3 className="font-semibold">{filename}</h3>
-                <p className="text-sm text-gray-500">
-                    Parsed Text:<br />{filepreview}
-                </p>
-            </div>
+        <div className="bg-white rounded-lg shadow p-4 space-y-2">
+            <h3 className="font-semibold">{filename}</h3>
+            <p className="text-sm text-gray-500">
+                Parsed Text:<br />{filepreview}
+            </p>
         </div>
     );
 }
